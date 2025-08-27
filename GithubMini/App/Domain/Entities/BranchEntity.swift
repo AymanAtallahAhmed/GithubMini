28815d1b1871bb279f1f0891b5844c346aab72b0
Ayman <princeofknights22@gmail.com>
2025-08-28 00:33:32 +0300

    create entities

---
+++ GithubMini/App/Domain/Entities/BranchEntity.swift
@@ -0,0 +1,13 @@
+//
+//  BranchEntity.swift
+//  GithubMini
+//
+//  Created by Ayman on 27/08/2025.
+//
+
+import Foundation
+
+struct BranchEntity: Identifiable {
+    let id = UUID()
+    let name: String
+}
---
+++ GithubMini/App/Domain/Entities/GitRepoEntity.swift
@@ -0,0 +1,14 @@
+//
+//  GitRepoEntity.swift
+//  GithubMini
+//
+//  Created by Ayman on 27/08/2025.
+//
+
+import Foundation
+
+struct GitRepoEntity: Identifiable {
+    let id: Int
+    let name: String
+    let language: String?
+}
