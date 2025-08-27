f1de1518b2b9387869039e4cbde04e9fdbb2af3e
Ayman <princeofknights22@gmail.com>
2025-08-27 20:33:05 +0300

    Initial Commit

---
+++ GithubMini/GithubMiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubMiniApp.swift
+//  GithubMini
+//
+//  Created by Ayman on 27/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct GithubMiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GithubMiniTests/GithubMiniTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubMiniTests.swift
+//  GithubMiniTests
+//
+//  Created by Ayman on 27/08/2025.
+//
+
+import Testing
+@testable import GithubMini
+
+struct GithubMiniTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
